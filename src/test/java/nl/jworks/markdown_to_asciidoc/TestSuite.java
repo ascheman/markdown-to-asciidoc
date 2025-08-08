d8c11adfe4cdd2a2d13196c4c1c1c0420097d8f2
Gerd Aschemann <gerd@aschemann.net>
2025-08-08 16:02:55 +0200

    #127 Do not ignore the most important test case
    I suggest to fix it and then incorporate the previous commit
    with a test case for #128.

--- src/test/java/nl/jworks/markdown_to_asciidoc/TestSuite.java
+++ src/test/java/nl/jworks/markdown_to_asciidoc/TestSuite.java
@@ -1,15 +1,10 @@
 package nl.jworks.markdown_to_asciidoc;
 
 import org.apache.commons.io.IOUtils;
-import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -18,7 +13,6 @@ import static org.junit.Assert.fail;
  * Converts the testsuite from https://github.com/karlcow/markdown-testsuite (run cat-all.py) to asciidoc, checks the output
  */
 public class TestSuite {
-    @Ignore
     @Test
     public void test() {
         String markDown = readToString("testsuite.md");
